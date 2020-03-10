687d9aed3cc5e05aa2607612945a170a704e6945
Alexander Artemenko <svetlyak.40wt@gmail.com>
2020-03-10 15:16:53 +0300

    Switched from Slynk to Swank to connect from the LEM.

--- main.lisp
+++ main.lisp
@@ -1,12 +1,14 @@
 (defpackage #:barista/main
   (:use #:cl)
-  (:import-from #:slynk)
+  ;; (:import-from #:slynk)
+  (:import-from #:swank)
   (:import-from #:barista/menu)
   (:import-from #:barista/plugin
                 #:get-available-plugins
                 #:start-plugin)
   (:export
-   #:load-plugins))
+   #:load-plugins
+   #:start-plugins))
 (in-package barista/main)
 
 
@@ -32,8 +34,19 @@
   ;;         (uiop:command-line-arguments))
   (log:config :sane2 :debug :daily "/tmp/barista.log")
 
-  (log:info "Starting SLYNK server")
-  (slynk:create-server :port 5007)
+  ;; (log:info "Starting SLYNK server")
+  ;; (slynk:create-server :port 5007 :dont-close t)
+
+  (log:info "Starting SWANK server")
+  (swank:create-server :port 5007 :dont-close t :style :spawn)
+
+  (load-plugins)
+  (start-plugins)
+
+  (when sleep
+    (log:info "Sleeping")
+    (loop (sleep 5))))
+
 
   (push "/Users/art/projects/lisp/cl-github/" asdf:*central-registry*)
   
