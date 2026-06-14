54247c4de0c4e2094426d6c2bb577364c1ac09c8
Alexander Artemenko <svetlyak.40wt@gmail.com>
2026-06-14 12:48:59 +0300

    Added a CI asdf system.

---
+++ src/ci.lisp
@@ -0,0 +1,38 @@
+(uiop:define-package #:barista-ci/ci
+  (:use #:cl)
+  (:import-from #:40ants-ci/jobs/linter)
+  (:import-from #:40ants-ci/jobs/run-tests
+                #:run-tests)
+  (:import-from #:40ants-ci/jobs/docs
+                #:build-docs)
+  (:import-from #:40ants-ci/workflow
+                #:defworkflow))
+(in-package #:barista-ci/ci)
+
+
+(defworkflow linter
+  :on-push-to "master"
+  :by-cron "0 10 * * 1"
+  :on-pull-request t
+  :cache t
+  :jobs ((40ants-ci/jobs/linter:linter
+          :asdf-systems ("barista"
+                         "barista-docs"
+                         "barista-plugins"))))
+
+(defworkflow docs
+  :on-push-to "master"
+  :by-cron "0 10 * * 1"
+  :on-pull-request t
+  :cache t
+  :jobs ((build-docs :asdf-system "barista-docs")))
+
+
+(defworkflow ci
+  :on-push-to "master"
+  :by-cron "0 10 * * 1"
+  :on-pull-request t
+  :cache t
+  :jobs ((run-tests
+          :asdf-system "barista"
+          :lisp ("sbcl-bin"))))
